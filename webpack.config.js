95c28273cdfa01904d2df8651c3e0f98ee1556a9
hayashi <kazuya.hayashi@g.hde.co.jp>
2018-09-21 10:54:54 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,78 +3,78 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const config = [
-    // For main process
-    {
-        entry: './src/main/main.ts',
-        output: {
-            path: path.resolve(__dirname, 'build/main'),
-            filename: 'main.js'
-        },
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|tsx)?$/,
-                    use: 'ts-loader',
-                    include: path.resolve(__dirname, 'src/'),
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        resolve: {
-            extensions: [ '.tsx', '.ts', '.js' ]
-        },
-        node: {
-            __filename: false,
-            __dirname: false,
-        },
-        target: 'electron-main',
+const mainProcessConfig = {
+    entry: './src/main/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'build/main'),
+        filename: 'main.js'
     },
-    // For renderer process
-    {
-        entry: './src/renderer/renderer.ts',
-        output: {
-            path: path.resolve(__dirname, 'build/renderer'),
-            filename: 'renderer.js'
-        },
-        devtool: "source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|tsx)?$/,
-                    include: path.resolve(__dirname, 'src/'),
-                    use: 'ts-loader',
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        resolve: {
-            extensions: [ '.tsx', '.ts', '.js' ]
-        },
-        target: 'electron-renderer',
-        plugins: [
-            new CopyWebpackPlugin([
-                {
-                    from: 'src/assets/img/',
-                    to: '../assets/img/'
-                },
-                {
-                    from: 'src/assets/style/',
-                    to: '../assets/style/'
-                }
-            ]),
-            new HtmlWebpackPlugin({
-                template: 'src/html/index.html',
-                filename: path.resolve(__dirname, 'build/html/index.html')
-            }),
-            new HtmlWebpackIncludeAssetsPlugin({
-                assets: ['assets/style/style.css'],
-                append: false,
-                publicPath: '../'
-            }),
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: 'ts-loader',
+                include: path.resolve(__dirname, 'src/'),
+                exclude: /node_modules/
+            }
         ]
-    }
-];
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    node: {
+        __filename: false,
+        __dirname: false,
+    },
+    target: 'electron-main',
+};
 
-module.exports = config;
+const rendererProcessConfig = {
+    entry: './src/renderer/renderer.ts',
+    output: {
+        path: path.resolve(__dirname, 'build/renderer'),
+        filename: 'renderer.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                include: path.resolve(__dirname, 'src/'),
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    target: 'electron-renderer',
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets/img/',
+                to: '../assets/img/'
+            },
+            {
+                from: 'src/assets/style/',
+                to: '../assets/style/'
+            }
+        ]),
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html',
+            filename: path.resolve(__dirname, 'build/html/index.html')
+        }),
+        new HtmlWebpackIncludeAssetsPlugin({
+            assets: ['assets/style/style.css'],
+            append: false,
+            publicPath: '../'
+        }),
+    ]
+};
+
+module.exports = [
+    mainProcessConfig,
+    rendererProcessConfig
+];
